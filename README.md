c40880e78f53ca35df6f50d1425593d82db8463c
Adesh <adeshkadambi@gmail.com>
2022-12-04 10:59:13 -0500

    implemented class to hold all predictions from shan and unidet

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# cdss-modeling
+# CDSS Modeling
+
+This repo contains all code for machine learning models pertaining to clinical decision support dashboard developed
+for hand function neurorehabilitation in outpatient settings. The primary focus is to classify activities of daily 
+living (ADLs) and instrumental activities of daily living (iADLs) that are informative of hand function at home.
+
+#### ADL/iADL categories include:
+- Feeding
+> Setting up, arranging, and bringing food [or fluid] from the plate or cup to the mouth; sometimes called self-feeding
+
+- Functional Mobility
+> Functional mobility Moving from one position or place to another (during performance of everyday activities), 
+> such as in-bed mobility, wheelchair mobility, and transfers (e.g., wheelchair, bed, car, shower, tub, toilet, 
+> chair, floor) . Includes functional ambulation and transportation of objects.
+
+- Grooming / Health Management
+> Obtaining and using supplies; removing body hair (e.g., using razor, tweezers, lotion); 
+> applying and removing cosmetics; washing, drying, combing, styling, brushing, and trimming hair; caring for nails 
+> (hands and feet); caring for skin, ears, eyes, and nose; applying deodorant; cleaning mouth; brushing and flossing 
+> teeth; and removing, cleaning, and reinserting dental orthotics and prosthetics. Developing, managing, and 
+> maintaining routines for health and wellness promotion, such as physical fitness, nutrition, decreased health 
+> risk behaviors, and medication routines.
+
+- Communication Management
+> Sending, receiving, and interpreting information using a variety of systems and equipment, 
+> including writing tools, telephones (cell phones or smartphones), keyboards, audiovisual recorders, computers or 
+> tablets, communication boards, call lights, emergency systems, Braille writers, telecommunication devices for deaf 
+> people, augmentative communication systems, and personal digital assistants.
+
+- Home Establishment and Management
+> Obtaining and maintaining personal and household possessions and environment (e.g., home, yard, garden, appliances, 
+> vehicles), including maintaining and repairing personal possessions (e .g ., clothing, household items) and knowing 
+> how to seek help or whom to contact.
+
+- Meal Preparation and Cleanup
+> Planning, preparing, and serving well-balanced, nutritious meals and cleaning up food and utensils after meals.
+
+- Leisure or Other Activities
+> Nonobligatory activity that is intrinsically motivated and engaged in during discretionary time, that is, time 
+> not committed to obligatory occupations or any of the aforementioned ADLs or iADLs.
+
+*Aota. (2020). Occupational Therapy Practice Framework: Domain & Process. 
+American Occupational Therapy Association, Incorporated.*
+
+---
+## Models
+```
+> 100DOH Model - hand object interactions and determination of active objects.
+
+Shan, D., Geng, J., Shu, M., & Fouhey, D. F. (2020). Understanding Human Hands in Contact at Internet Scale. 
+In arXiv [cs.CV]. arXiv. http://arxiv.org/abs/2006.06669
+
+> Unidet Model - classification and localization of everyday objects related to functional activity.
+
+Zhou, X., Koltun, V., & Krähenbühl, P. (2021). Simple multi-dataset detection. 
+In arXiv [cs.CV]. arXiv. http://arxiv.org/abs/2102.13086
+```
---
+++ image.py
@@ -0,0 +1,110 @@
+import numpy as np
+import torch
+import torchvision
+
+
+class ImageContainer:
+    """
+    Object that stores the file path of the image, the ground truth labels, and shan/unidet predictions.
+    """
+
+    def __init__(self) -> None:
+        self.name = None
+        self.gt_boxes, self.gt_classes, self.gt_active = None, None, None
+        self.unidet_boxes, self.unidet_classes, self.unidet_scores, self.unidet_active = None, None, None, None
+        self.shan_boxes, self.shan_scores = None, None
+
+    def __repr__(self) -> str:
+        return f"{self.name}: {vars(self)}"
+
+    def __str__(self) -> str:
+        return f"{self.name}: {vars(self)}"
+
+    def get_ground_truth(self, ground_truth_data, ontology):
+        # sourcery skip: raise-specific-error
+        """
+        Checks if image was skipped in the ground truth data, if not, then it gets the ground truth labels.
+        """
+        self.name = ground_truth_data["External ID"].split(".")[0]
+        self.gt_boxes, self.gt_classes = [], []
+
+        if not ground_truth_data["Skipped"]:
+            for gt in ground_truth_data["Label"]["objects"]:
+                try:
+                    bbox = [
+                        gt["bbox"]["left"],
+                        gt["bbox"]["top"],
+                        gt["bbox"]["left"] + gt["bbox"]["width"],
+                        gt["bbox"]["top"] + gt["bbox"]["height"],
+                    ]
+                    self.gt_boxes.append(bbox)
+                    self.gt_classes.append(ontology[gt["title"]])
+                except:
+                    raise Exception(
+                        f'Error in {self.name} -- {gt["title"]} not found in ontology.'
+                    )
+
+        self.gt_boxes, self.gt_classes = np.array(self.gt_boxes, dtype="int"), np.array(
+            self.gt_classes
+        )
+
+    def get_unidet_results(self, unidet_data):
+        """
+        Get the unidet predictions for the image.
+        """
+        self.unidet_boxes, self.unidet_classes, self.unidet_scores = (
+            np.array(unidet_data["boxes"], dtype=int),
+            np.array(unidet_data["classes"]),
+            np.array(unidet_data["scores"]),
+        )
+
+    def get_shan_results(self, shan_data):
+        """
+        Get the shan predictions for the image
+        """
+        shan_boxes, shan_scores = self.parse_shan_results(shan_data)
+        self.shan_boxes, self.shan_scores = np.array(shan_boxes, dtype=int), np.array(
+            shan_scores
+        )
+
+    def parse_shan_results(self, shan_data):
+        """
+        Parse the shan predictions for the image.
+
+        args: shan_data (dict) in format:
+        {
+            objects: [[boxes(4), score(1), state(1), offset_vector(3), left/right(1)], ...]],
+            hands: [[boxes(4), score(1), state(1), offset_vector(3), left/right(1)], ...]],
+        }
+
+        returns: shan_boxes (list), shan_scores (list)
+        """
+        shan_boxes, shan_scores = [], []
+
+        if shan_data["objects"] is not None:
+            for obj in shan_data["objects"]:
+                shan_boxes.append(obj[:4])
+                shan_scores.append(obj[4])
+
+        return shan_boxes, shan_scores
+
+    def detect_active_objects(self, threshold=0.75):
+        """
+        Detects the active objects in the ground truth and unidet predictions by using the shan model as "ground truth".
+        """
+        self.gt_active = np.zeros(len(self.gt_boxes))
+        self.unidet_active = np.zeros(len(self.unidet_boxes))
+
+        if len(self.shan_boxes) > 0 and len(self.unidet_boxes) > 0 and len(self.gt_boxes) > 0:
+            gt_boxes = torch.from_numpy(self.gt_boxes)
+            unidet_boxes = torch.from_numpy(self.unidet_boxes)
+            shan_boxes = torch.from_numpy(self.shan_boxes)
+
+            ious_gt = torchvision.ops.box_iou(gt_boxes, shan_boxes).numpy()
+            ious_unidet = torchvision.ops.box_iou(unidet_boxes, shan_boxes).numpy()
+
+            gt_threshold = np.argwhere(ious_gt > threshold)[:, 0]
+            unidet_threshold = np.argwhere(ious_unidet > threshold)[:, 0]
+
+            self.gt_active[gt_threshold] = 1
+            self.unidet_active[unidet_threshold] = 1
