23728ca78b07f6581b4e29e939ca9557883f91ba
Adesh <adeshkadambi@gmail.com>
2023-02-02 22:22:09 +0000

    pytorch dataset from labelbox labels

--- dataset.py
+++ dataset.py
@@ -1,17 +1,89 @@
-'''Make a PyTorch Dataset for the Home Data Eval Subset.'''
+"""Make a PyTorch Dataset for the Home Data Eval Subset."""
 
 import os
-import pandas as pd # type: ignore
+import torch  # type: ignore
 
-from torchvision.io import read_image # type: ignore
-from torch.utils.data import Dataset # type: ignore
+from PIL import Image  # type: ignore
+from torch.utils.data import Dataset  # type: ignore
+
 
 class HomeDataEvalSubset(Dataset):
-    def __init__(self, root_dir, annotations, ontology):
-        pass
+    def __init__(self, root: str, mapper: dict, annotations: list[dict]):
+        self.root = root
+        self.mapper = mapper
+        self.annotations = annotations
+
+        # get list of all images in annotations
+        img_names = [img["External ID"] for img in annotations]
+
+        # get path for each img in img_names in all subdirectories of root
+        images: list[str] = []
+        for root, _, files in os.walk(self.root):
+            for file in files:
+                if file in img_names:
+                    images.append(os.path.join(root, file))
+
+        self.images = images
 
     def __len__(self):
-        pass
+        return len(self.images)
 
-    def __getitem__(self):
-        pass
+    def __getitem__(self, idx: int):
+        # load image
+        img_path = self.images[idx]
+        img = Image.open(img_path).convert("RGB")
+
+        # get annotation for img
+        img_name = os.path.basename(img_path)
+        annotation = [
+            img for img in self.annotations if img["External ID"] == img_name
+        ][0]
+
+        # get boxes and labels
+        boxes = []
+        labels = []
+
+        if not annotation["Skipped"]:
+            for gt in annotation["Label"]["objects"]:
+                box = [
+                    gt["bbox"]["left"],
+                    gt["bbox"]["top"],
+                    gt["bbox"]["left"] + gt["bbox"]["width"],
+                    gt["bbox"]["top"] + gt["bbox"]["height"],
+                ]
+                boxes.append(box)
+                labels.append(gt["title"])
+
+        # remap labels
+        labels = self.remap_labels(labels, self.mapper)
+
+        # generate target dict
+        target = {}
+        target["image_id"] = torch.tensor([idx])
+        target["boxes"] = torch.as_tensor(boxes, dtype=torch.float32)
+        target["labels"] = torch.as_tensor(labels, dtype=torch.int64)
+        target["iscrowd"] = torch.zeros((len(boxes),), dtype=torch.int64)
+
+        # calculate area of each box, if there are no boxes then make empty tensor
+        if len(boxes) > 0:
+            area = []
+            for box in boxes:
+                area.append((box[2] - box[0]) * (box[3] - box[1]))
+            target["area"] = torch.as_tensor(area, dtype=torch.float32)
+        else:
+            target["area"] = torch.as_tensor([], dtype=torch.float32)
+
+        return img, target
+
+    @staticmethod
+    def remap_labels(labels: list[str], mapper: dict) -> list[int]:
+        """Remap labels to match the labels in the mapper."""
+
+        labels = labels.copy()
+
+        for i, label in enumerate(labels):
+            for key, value in mapper.items():
+                if label in value["subclasses_names"]:
+                    labels[i] = key
+
+        return [int(label) for label in labels]
--- scratch.py
+++ scratch.py
@@ -1,50 +1,21 @@
 """This is a scratch file for testing out code snippets."""
 
-import numpy as np
-import pandas as pd # type: ignore
 import pickle
-import draw
-import cv2 # type: ignore
+import json
 
-# test draw module using images in datasets/home-data-eval folder
+from dataset import HomeDataEvalSubset
 
-# # load image containers from pickle file
-# with open("datasets/home-data-eval/results_remapped.pkl", "rb") as f:
-#     images = pickle.load(f)
+# set root directory
+root = "datasets/home-data-eval/"
 
-# print('Loaded Data')
+# load mapper from pickle file
+mapper = pickle.load(open("datasets/mapper.pkl", "rb"))
 
-# # load mapper from pickle file
-# with open("datasets/home-data-eval/mapper.pkl", "rb") as f:
-#     mapper = pickle.load(f)
+# load annotations from json file
+annotations = json.load(open("datasets/ground_truth_labels.json"))
 
-# # # remap classes
-# # for img in images:
-# #   img.remap_classes(mapper['Subclass ID'])
+# create dataset
+data = HomeDataEvalSubset(root, mapper, annotations)
 
-# # # save remapped classes to pickle file
-# # with open("datasets/home-data-eval/results_remapped.pkl", "wb") as f:
-# #     pickle.dump(images, f)
-
-# print('Loaded Mapper & Remapped Classes')
-
-# # draw bounding boxes on images and save to tests/ folder
-# imgs = draw.batch_draw_bbox_image_container(images[:1], "datasets/home-data-eval", mapper, "/workspaces/cdss-modeling/tests")
-
-# # plot images
-
-# cv2.imshow('image', imgs[0])
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-# random numpy array of 1 and 0
-a = np.random.randint(2, size=(4))
-# random numpy array of letters
-b = np.random.choice(list('abcdefghijklmnopqrstuvwxyz'), size=(4))
-
-print(a)
-print(b)
-
-print(a[a != 0])
-print(b[a != 0])
+# test dataset
+print(data[0])
